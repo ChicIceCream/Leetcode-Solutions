7ab03a1ad043195bad3fc8e7ce9f9fe07b4f81fb
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2023-11-25 17:37:15 +0530

    Fixed code and added comments

--- Binary Search/367 - Valid Perfect Square.py
+++ Binary Search/367 - Valid Perfect Square.py
@@ -1,19 +1,28 @@
+# Wouldnt work here because Leetcode has its own test cases
+# To make it work, change the function according to you liking
+
+# Declaring class
 class Solution:
-   def isPerfectSquare(self, num: int) -> bool:
-    if num < 0:
+    #Declaring function
+    def isPerfectSquare(self, num: int) -> bool:
+        
+        # To make sure the number is greater than 0
+        if num < 0:
+            return False
+
+        # Assinging Variables
+        left, right = 0, num
+
+        # Start of loop
+        while left <= right:
+            mid = (left + right) // 2
+            squared = mid * mid
+
+            if squared == num:
+                return True
+            elif squared < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+
         return False
-
-    left, right = 0, num
-
-    while left <= right:
-        mid = (left + right) // 2
-        squared = mid * mid
-
-        if squared == num:
-            return True
-        elif squared < num:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return False
