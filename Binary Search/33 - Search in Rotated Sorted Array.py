26fe99a8e04ea7274692478c298ee25e3b805d83
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2023-11-27 21:11:56 +0530

    Fixed Code

--- Binary Search/33 - Search in Rotated Sorted Array.py
+++ Binary Search/33 - Search in Rotated Sorted Array.py
@@ -1,10 +1,11 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        # Declaring variables
         left, right = 0, len(nums) - 1
 
         while left <= right:
             mid = (left + right) // 2 
-
+            
             if target == nums[mid]:
                 return mid
 
