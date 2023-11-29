47947276e36e23ce660cbd20a1716be4ea1b10ce
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2023-11-30 05:02:06 +0530

    Solved More

---
+++ Binary Search/35.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        upper_bound, lower_bound = len(nums) - 1, 0
+
+        while lower_bound <= upper_bound:
+            index = (upper_bound + lower_bound) // 2
+
+            if target == nums[index]:
+                return index 
+            elif target > nums[index]:
+                lower_bound = index + 1
+            else:
+                upper_bound = index - 1
+        return lower_bound
+
+    
