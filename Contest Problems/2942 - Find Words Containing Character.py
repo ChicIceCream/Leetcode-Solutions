a96fa3de546ece8d7780ac9c58e9ed97a73fda90
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2023-12-05 00:30:19 +0530

    Fixed Code

--- Contest Problems/2942 - Find Words Containing Character.py
+++ Contest Problems/2942 - Find Words Containing Character.py
@@ -1,6 +1,7 @@
 # You are given a 0-indexed array of strings words and a character x.
 # Return an array of indices representing the words that contain the character x.
 # Note that the returned array may be in any order.
+# Uses simple for looping and if function
 
 class Solution:
     def findWordsContaining(self, words: List[str], x: str) -> List[int]:
