aacbf4dcecd6bc9c3ed7f32cc9abfd28ec49b0f7
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-01-02 10:32:29 +0530

    todays daily

---
+++ Normal Problems/2610 - Convert an Array Into a 2D Array With Conditions.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        
+        # freq should be a list of size nums + 1
+        freq = [0] * (len(nums) + 1)
+        ans = []
+
+        for c in nums:
+
+            if freq[c] >= len(ans):
+                ans.append([])
+                
+            ans[freq[c]].append(c)
+            freq[c] += 1
+
+        return ans
