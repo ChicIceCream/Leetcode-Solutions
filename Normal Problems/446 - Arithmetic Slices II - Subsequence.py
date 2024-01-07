0f2ea228d2e5fe967d2261492491feb5f1c80c66
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-01-08 01:39:11 +0530

    fixed location

---
+++ Normal Problems/446 - Arithmetic Slices II - Subsequence.py
@@ -0,0 +1,17 @@
+
+
+class Solution:
+    def numberOfArithmeticSlices(self, nums):
+        n = len(nums)
+        count = 0
+        dp = [{} for _ in range(n)] 
+
+        for i in range(n):
+            for j in range(i):
+                diff = nums[i] - nums[j]
+                if diff in dp[j]:
+                    dp[i][diff] = dp[i].get(diff, 0) + dp[j][diff]
+                    count += dp[j][diff] 
+                dp[i][diff] = dp[i].get(diff, 0) + 1
+
+        return count
