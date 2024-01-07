a6a496efb22393f517ccb36a9cadeb5df896e19d
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-01-07 23:59:31 +0530

    more

---
+++ Contest Problems/2706 - Buy Two Chocolates.py
@@ -0,0 +1,36 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        def merge_sort(arr):
+            if len(arr) <= 1:
+                return arr
+            
+            mid = len(arr) // 2
+            l_half = arr[:mid]
+            r_half = arr[mid:]
+            l_half = merge_sort(l_half)
+            r_half = merge_sort(r_half)
+
+            return merge(l_half, r_half)
+
+        def merge(left, right):
+            new = []
+            i,j = 0,0
+
+            while i < len(left) and j < len(right):
+                if left[i] < right[j]:
+                    new.append(left[i])
+                    i += 1
+                else:
+                    new.append(right[j])
+                    j += 1
+                
+            new.extend(left[i:])
+            new.extend(right[j:])
+            return new
+
+        sorted_prices = merge_sort(prices)
+        price = sorted_prices[0] + sorted_prices[1]
+        if price > money:
+            return money
+        else:
+            return money - price
