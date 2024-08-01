9bba9ae1c71c5de1b3e130b0e6ed281fb9fafac2
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-08-01 23:46:44 +0530

    almost

--- Normal Problems/682 - Baseball Game.py
+++ Normal Problems/682 - Baseball Game.py
@@ -1,16 +1,17 @@
 stack = []
-operations = ["5","2","C","D","+"]
+
+operations = ["5","-2","4","C","D","9","+","+"]
+
 for op in operations:
-    if op.isdigit():
-        stack.append(int(op))
+    if op == '+':
         print(stack)
-    elif op == '+':
-        stack.append(op)
-        print(stack)
-        stack.append(stack[-1] + stack[-2])
+        stack.append(int(stack[-2]) + int(stack[-1]))
     elif op == 'D':
-        stack.append(stack[-1]**2)
+        stack.append(int(stack[-1])*2)
         print(stack)
     elif op == 'C':
         stack.pop()
+        print(stack)
+    else:
+        stack.append(int(op))
         print(stack)
