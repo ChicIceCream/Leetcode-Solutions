37f6c6b43b0e2300a725c0a639d9581ee318bc01
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-08-12 23:38:33 +0530

    back to searching

---
+++ Contest Problems/74 -Search a 2D Matrix.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        top, bot = 0, ROWS - 1
+
+        while top <= bot:
+            row = (top + bot) // 2
+
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+        
+        if not (top <= bot):
+            return False
+        
+        row = (top + bot) // 2
+
+        left, right = 0, COLS - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if target > matrix[row][mid]:
+                left = mid + 1
+            elif target < matrix[row][mid]:
+                right = mid - 1
+            else: 
+                return True
+        
+        return False
