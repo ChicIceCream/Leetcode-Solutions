aef0ef1cdc658afe2eda6ea3489df3e4ccb84080
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-11-28 14:31:50 +0530

    famous problem

--- Normal Problems/121 - Best Time to Buy and Sell Stock.py
+++ Normal Problems/121 - Best Time to Buy and Sell Stock.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: List[int]) -> int: # type: ignore
         buy, sell = 0, 1
         max_profit = 0
         # repeat until the sell pointer reaches the end
@@ -17,3 +17,19 @@ class Solution:
             sell += 1
         
         return max_profit
+
+########################################### Using Dunamic Programming ################################################
+
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         max_profit = 0  # Tracks the highest profit
+#         min_price = prices[0]  # Tracks the lowest price seen so far
+
+#         for current_price in prices:
+#             # Calculate profit if sold at current_price
+#             max_profit = max(max_profit, current_price - min_price)
+#             # Update min_price if current_price is lower
+#             min_price = min(min_price, current_price)
+        
+#         return max_profit
