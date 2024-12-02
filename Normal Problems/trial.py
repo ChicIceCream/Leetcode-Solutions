a230c1c628bf8821061e274611f845f46bc262b9
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-12-02 23:45:43 +0530

    working code

--- Normal Problems/trial.py
+++ Normal Problems/trial.py
@@ -1,3 +1,12 @@
-word_list = ['i', 'love', 'eating', 'burger']
-
-print(word_list[1][:3])
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        word_list = list(sentence.split())
+        print(word_list)
+        for i in range(len(word_list)):
+            print(i, word_list[i])
+            searchWordLength = len(searchWord)
+            if searchWord in word_list[i][:searchWordLength]:
+                return i + 1
+            else:
+                i + 1
+        return -1
