9893f40321366c57f37e46b4584d61dde392b674
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-12-02 23:47:30 +0530

    todays problem

--- Normal Problems/1455 - Check If a Word Occurs As a Prefix of Any Word in a Sentence.py
+++ Normal Problems/1455 - Check If a Word Occurs As a Prefix of Any Word in a Sentence.py
@@ -1,9 +1,11 @@
 class Solution:
     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        # split up the words so only words print, without this part, it will give every letter instead
         word_list = list(sentence.split())
-        print(word_list)
+        # print(word_list)
         for i in range(len(word_list)):
-            print(i, word_list[i])
+            # print(i, word_list[i])
+            # to search through only the prefix part
             searchWordLength = len(searchWord)
             if searchWord in word_list[i][:searchWordLength]:
                 return i + 1
