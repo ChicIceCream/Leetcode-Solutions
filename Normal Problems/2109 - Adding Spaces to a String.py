b985756697ed2c4391d9287491e36c5f1d88a928
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-12-04 23:30:16 +0530

    late

---
+++ Normal Problems/2109 - Adding Spaces to a String.py
@@ -0,0 +1,12 @@
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        final_list = []
+        prev_index = 0 # to know where to start the indexing for each string
+
+        for i in range(len(spaces)):
+            final_list.append(s[prev_index:spaces[i]]) # adds all the letters from the prev_index to the current index
+            prev_index = spaces[i] # update the prev_index to new index
+            # print(final_list)  
+        
+        final_list.append(s[prev_index:])
+        return " ".join(final_list)
