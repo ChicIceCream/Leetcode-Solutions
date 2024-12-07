1d58a51295c9f4b376ef875eff295af3b445d174
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-12-07 23:39:36 +0530

    boom

---
+++ Normal Problems/283 - Move Zeroes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                # swap right and left elements
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1  # move the pointer 
