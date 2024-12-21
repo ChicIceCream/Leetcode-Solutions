0d7a4755186583ea5475935aea45e463a76524e1
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-12-21 23:10:48 +0530

    another way

--- Normal Problems/258 - Add Digits.py
+++ Normal Problems/258 - Add Digits.py
@@ -5,3 +5,9 @@ class Solution:
         return num
 
 ########################### Another smart one ############################################
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        return 1 + (num - 1) % 9
