5cc30b25d747c91204b56fd294c4ae27891781c6
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2025-02-01 16:44:01 +0530

    original way

--- Normal Problems/258 - Add Digits.py
+++ Normal Problems/258 - Add Digits.py
@@ -1,5 +1,14 @@
 class Solution:
     def addDigits(self, num: int) -> int:
+        while len(str(num)) > 1:
+            num_list = [int(digit) for digit in str(num)]
+            num = sum(num_list)
+        return num
+
+############################# Another way ###################################################
+
+class Solution:
+    def addDigits(self, num: int) -> int:
         while num >= 10:  
             num = sum(int(digit) for digit in str(num))  
         return num
