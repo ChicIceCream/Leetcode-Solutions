eae28125384a68a91a4300e93e76c75ea2769577
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2025-02-01 16:44:58 +0530

    ignoring

--- Normal Problems/2177 - Find Three Consecutive Integers That Sum to a Given Number.py
+++ Normal Problems/2177 - Find Three Consecutive Integers That Sum to a Given Number.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def sumOfThree(self, num: int) -> List[int]:
+    def sumOfThree(self, num: int) -> List[int]: #type:ignore
         if num % 3 == 0:
             num //= 3
             return [num - 1, num, num + 1]
