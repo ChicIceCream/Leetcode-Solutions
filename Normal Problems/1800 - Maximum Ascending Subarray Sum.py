a5eb49ee5dc9ede6dbe0eeb6a098515d3f9a6012
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2025-02-05 00:42:27 +0530

    fun

---
+++ Normal Problems/1800 - Maximum Ascending Subarray Sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+
+        cur_streak = maxi = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                cur_streak += nums[i]
+            else:
+                cur_streak = nums[i]
+            maxi = max(maxi, cur_streak)
+        return maxi
---
+++ Normal Problems/3105 - Longest Strictly Increasing or Strictly Decreasing Subarray.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        maxi = 1
+        current_streak = 1
+        back_streak = 1
+        for i in range(len(nums) - 1):
+            if nums[i] < nums[i + 1]:
+                current_streak += 1
+                if maxi < current_streak:
+                    maxi = current_streak
+            else:
+                current_streak = 1
+            
+            if nums[i] > nums[i + 1]:
+                back_streak += 1
+                if maxi < back_streak:
+                    maxi = back_streak
+            else:
+                back_streak = 1
+            #print(i, i+1, back_streak, maxi)
+        return maxi
